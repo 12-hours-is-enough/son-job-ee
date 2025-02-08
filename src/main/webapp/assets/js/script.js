3c13c0185a79c6a92ae3937dc980199c4e3393df
nahong_c <evangelion14125@gmail.com>
2025-02-08 17:26:56 +0900

    ✨ Add Login page invalidate

--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -1,10 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let table = document.getElementById("jobTable");
-
-    table.addEventListener("click", function (event) {
-        let target = event.target;
-        if (target.tagName === "TD") {
-            alert("선택한 직무 ID: " + target.parentElement.cells[0].innerText);
-        }
-    });
+// 회원가입 버튼 클릭 시 userType 값이 선택되지 않았으면 경고
+var signupLink = document.querySelector('.signup-link');
+signupLink.addEventListener('click', function(event) {
+    event.preventDefault(); // 기본 동작 막기
+    var userTypeValue = document.querySelector('input[name="userType"]:checked');
+    if (userTypeValue) {
+        document.getElementById('userTypeHidden').value = userTypeValue.value;
+        document.getElementById('signupForm').submit();
+    } else {
+        alert('회원가입 전에 구인자 또는 구직자 유형을 선택해주세요.');
+    }
 });
