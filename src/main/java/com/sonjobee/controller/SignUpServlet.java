97258802a2cc9f33863466a4895d9bcda636da60
nahong_c <evangelion14125@gmail.com>
2025-02-08 17:28:20 +0900

    ✨ Add Login, Signup

--- src/main/java/com/sonjobee/controller/SignUpServlet.java
+++ src/main/java/com/sonjobee/controller/SignUpServlet.java
@@ -1,41 +1,73 @@
 package com.sonjobee.controller;
 
+import java.io.IOException;
+
+import com.sonjobee.dao.CompanyDAO;
+import com.sonjobee.dao.UserDAO;
+import com.sonjobee.model.Company;
+import com.sonjobee.model.User;
+import com.sonjobee.util.TimestampConverter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class SignUpServlet
- */
 @WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignUpServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+	
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+    	String userType = request.getParameter("userType");
+		if (session.getAttribute("islogin") != null) {
+			response.sendRedirect("job");
+		} else {
+			if (userType.equals("user")) {
+				request.getRequestDispatcher("/WEB-INF/views/userSignup.jsp").forward(request, response);
+			} else if(userType.equals("company")) {
+				request.getRequestDispatcher("/WEB-INF/views/companySignup.jsp").forward(request, response);
+			}
+		}
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String userType = request.getParameter("usertype");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+    	try {
+    		if (userType.equals("user")) {
+        		User user = new User();
+        		
+        		user.setEmail(request.getParameter("userEmail"));
+        		user.setPassword("userPw");
+        		user.setName(request.getParameter("userName"));
+        		user.setPhone(request.getParameter("userPhone"));
+        		user.setBirthDate(TimestampConverter.convertStringToDate(request.getParameter("userBirth")));
+        		user.setGender(request.getParameter("userGender"));
+        		user.setExperience(request.getParameter("userExperience"));
+        		
+        		UserDAO.userSign(user);
+        		
+    			response.sendRedirect("login");
+        	} else if (userType.equals("company")) {
+        		Company company = new Company();
+        		
+        		company.setEmail(request.getParameter("companyEmail"));
+        		company.setName(request.getParameter("companyName"));
+        		company.setPhone(request.getParameter("companyPhone"));
+        		company.setPassword(request.getParameter("companyPw"));
+        		
+        		CompanyDAO.signUpCompany(company);
+        		
+    			response.sendRedirect("login");
+        	}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	
 	}
-
 }
