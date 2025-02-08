21677df17c2e72d9c85b65fe7bb0cf61423cdbdb
nahong_c <evangelion14125@gmail.com>
2025-02-08 22:59:44 +0900

    ✨ Add apply

--- src/main/java/com/sonjobee/controller/BoardServlet.java
+++ src/main/java/com/sonjobee/controller/BoardServlet.java
@@ -35,35 +35,52 @@ public class BoardServlet extends HttpServlet {
 	// userDAO에서 받아온 Job리스트를 myStatus.jsp로 전달,
 	// 공고 현황 공고 현황 누르면 companyID 값 받아와서 companyDAO로 전달 
 	// company DAO에서 받아온 Job리스트를 mystatus로 전달
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
-		// id 값
-		int id = (Integer)session.getAttribute("id");
-		// 유저 타입
-		String userType = (String) session.getAttribute("usertype");
 		
-		if(userType == "user") {
-			id = Integer.parseInt(request.getParameter("userId"));
-
+		if (session.getAttribute("islogin") == null) {
+			response.sendRedirect("login");
+			return ;
+		} 
+		
+		int id = (Integer)session.getAttribute("id");  // id 값
+		String userType = (String) session.getAttribute("usertype");  // 유저 타입
+		
+		if(userType.equals("user")) {
 			// jobDAO에서 해당 jobId 리스트로 Job 전체 정보 가져오기
 			List<Job> jobList = jobDAO.getAppliedJobs(id);
 
 			request.setAttribute("jobList", jobList);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("myStatus.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/myStatus.jsp");
 			dispatcher.forward(request, response);
 		}
-		else if (userType == "company") {
+		else if (userType.equals("company")) {
 			// jobDAO에서 해당 jobId 리스트로 Job 전체 정보 가져오기
 			List<Job> jobList = jobDAO.getCompanyJob(id);
 
 			request.setAttribute("jobList", jobList);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("myPosts.jsp");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/myPosts.jsp");
 			dispatcher.forward(request, response);
 		}
 		
 	}
-
-
+	  
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	HttpSession session = request.getSession();
+    	
+		if (session.getAttribute("islogin") == null) {
+			response.sendRedirect("login");
+			return ;
+		} 
+		
+		int userId = (Integer) session.getAttribute("id"); 
+		int jobId = Integer.parseInt(request.getParameter("jobId"));
+		
+		userDAO.updateUserAppliedList(userId, jobId);
+	}
 }
